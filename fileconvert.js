bbb74e4428bc4193e80e395229b9e7a9d3506a31
felipeeths <ethszx@gmail.com>
2017-06-03 17:05:30 -0300

    fixed imagen src

--- fileconvert.js
+++ fileconvert.js
@@ -6,7 +6,7 @@ window.onload = function() {
       };
       $.each(data, function(i, f) {
                var tblRow = "<tr>" + "<td>" + f.id + "</td>" +
-                "<td>" + f.name + "</td>" + "<td>" + f.price + "</td>" + "<td>" + f.images + "</td>" + "</tr>"
+                "<td>" + f.name + "</td>" + "<td>" + f.price + "</td>" + "<td>" + f.images[0].src + "</td>" + "</tr>"
                 $(tblRow).appendTo("#userdata tbody");
                 json.produto.push({
                   "agrupador" : f.id,
@@ -23,11 +23,25 @@ window.onload = function() {
                   "id_oferta": "",
                   "imagens": {
                     "imagem": f.images[0].src
-                  }
-                });
-
+                  },
+                   "categoria": f.categories[0].name,
+                   "cod_barra": "",
+              		 "disponibilidade": f.stock_quantity,
+              		 "altura": f.dimensions.height,
+              		 "comprimento": f.dimensions.width,
+              		 "largura": f.dimensions.length,
+              		 "peso": f.weight,
+                   "especificacoes":{
+                     "especificacao": f.attributes[0]
+                   },
+                   "atributos":{
+                     "atributo":{
+                       "nome": "",
+                       "valor":""
+                     }
+                   }
+                  });
+        });
+        console.log(JSON.stringify(json));
     });
-    console.log(JSON.stringify(json));
-  });
-
-}
+  };
