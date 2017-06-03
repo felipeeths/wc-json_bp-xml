cb45c3f8b0ded2cc4813b681ef346ec786f45e13
felipeeths <ethszx@gmail.com>
2017-06-03 15:54:19 -0300

    first commit

---
+++ fileconvert.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+    $.getJSON('produtosnewexp.json', function(data) {
+      var json = {
+        produto: []
+      };
+      $.each(data, function(i, f) {
+               var tblRow = "<tr>" + "<td>" + f.id + "</td>" +
+                "<td>" + f.name + "</td>" + "<td>" + f.price + "</td>" + "<td>" + f.images + "</td>" + "</tr>"
+                $(tblRow).appendTo("#userdata tbody");
+                json.produto.push({
+                  "agrupador" : f.id,
+                  "titulo" : f.name,
+                  "descricao" : f.short_description,
+                  "canal_buscape" : {
+                    "canal_url": f.permalink,
+                    "valores":{
+                      "forma_de_pagamento": "pagseguro",
+                      "pacelamento": "",
+                      "canal_preco": f.price
+                    }
+                  },
+                  "id_oferta": "",
+                  "imagens": {
+                    "imagem": f.images["src"]
+                  }
+                });
+
+    });
+    console.log(JSON.stringify(json));
+  });
+
+}
