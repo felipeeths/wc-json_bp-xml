85c2e70dea2d7eb4b8863da776a1b20cbf56da33
felipeeths <ethszx@gmail.com>
2017-06-05 08:34:28 -0300

    as

--- fileconvert.js
+++ fileconvert.js
@@ -4,7 +4,9 @@ window.onload = function() {
       var json = {
         produto: []
       };
+      var value = 0;
       $.each(data, function(i, f) {
+
                var tblRow = "<tr>" + "<td>" + f.id + "</td>" +
                 "<td>" + f.name + "</td>" + "<td>" + f.price + "</td>" + "<td>" + f.images[0].src + "</td>" + "</tr>"
                 $(tblRow).appendTo("#userdata tbody");
@@ -21,9 +23,9 @@ window.onload = function() {
                     }
                   },
                   "id_oferta": "",
-                  "imagens": {
-                    "imagem": f.images[0].src
-                  },
+                  "imagens": [{
+
+                  }],
                    "categoria": f.categories[0].name,
                    "cod_barra": "",
               		 "disponibilidade": f.stock_quantity,
@@ -31,16 +33,42 @@ window.onload = function() {
               		 "comprimento": f.dimensions.width,
               		 "largura": f.dimensions.length,
               		 "peso": f.weight,
-                   "especificacoes":{
-                     "especificacao": f.attributes[0]
-                   },
-                   "atributos":{
+                   "especificacoes":[{
+                     "especificacao":{
+                       "nome": "",
+                       "valor":""
+                     }
+                   }],
+                   "atributos":[{
                      "atributo":{
                        "nome": "",
                        "valor":""
                      }
-                   }
+                   }]
                   });
+
+                $.each(f.images,function(i,f){
+                  json.produto[value].imagens.push({
+                    "imagem": f.src
+                  });
+                });
+
+                $.each(f.attributes,function(i,f){
+                  var valor = "";
+                  $.each(f.options,function(i,f){
+                    if(i+1 < f.length){
+                      valor += f + "/";
+                    }else {
+                      valor += f;
+                      }
+                  });
+                  json.produto[value].especificacoes.especificacao.push({
+                    "nome": f.name,
+                    "valor": valor
+                  });
+                });
+
+                value++;
         });
         console.log(JSON.stringify(json));
     });
