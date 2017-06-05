b686b4c71497bb6a6622f5b70d2daf8c3660fa6c
Ingo Guilherme Both Eyng <Ingo Guilherme Both Eyng>
2017-06-05 15:17:28 -0300

    Adicionando arquivo XML para teste

--- fileconvert.js
+++ fileconvert.js
@@ -24,6 +24,7 @@ window.onload = function() {
                         "pacelamento": "",
                         "canal_preco": f.price
                       }]
+
                     }
                   },
                   "id_oferta": "",
