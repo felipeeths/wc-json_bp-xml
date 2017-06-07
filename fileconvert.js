6030c67a7a1940fe861935d600b2ec6f21125b75
Ingo Guilherme Both Eyng <ingo_eyng_1992@hotmail.com>
2017-06-07 19:29:25 -0300

    Adicionado bibliotecas OAuth; Correções no código

--- fileconvert.js
+++ fileconvert.js
@@ -1,9 +1,32 @@
-String.prototype.replaceAll = function(search, replacement) {
-  var target = this;
-  return target.replace(new RegExp(search, 'g'), replacement);
-};
+$(document).ready(function () {
+    $('#form').on('submit', function(e) {
+        e.preventDefault();
 
-window.onload = function() {
+        $.ajax({
+            url : "getjson.php",
+            type: "POST",
+            data: "url=" + $("#url").val() + "&ck=" + $("#ck").val() + "&cs=" + $("#cs").val() + "&cmd=" + $("#cmd").val(),
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    });
+});
+
+function convertJSon2XML(json) {
+    var x2js = new X2JS();
+
+    var xml = {
+      root: json
+    };
+
+    return x2js.json2xml_str(xml);
+}
+
+function convertWooToBuscape() {
     $.getJSON('produtosnewexp.json', function(data) {
       var json = {
         produto: []
@@ -14,7 +37,12 @@ window.onload = function() {
 
                var tblRow = "<tr>" + "<td>" + f.id + "</td>" +
                 "<td>" + f.name + "</td>" + "<td>" + f.price + "</td>" + "<td>" + f.images[0].src + "</td>" + "</tr>"
-                
+
+                var valor_total = parseFloat(f.price);
+                var valor_parcela_cartao_3x_sem_juros = valor_total / 3;
+                var valor_parcela_cartao_12x_com_juros = valor_total * 0.10040;
+                var valor_total_cartao_12x_com_juros = valor_parcela_cartao_12x_com_juros * 12;
+
                 $(tblRow).appendTo("#userdata tbody");
                 json.produto.push({
                   "agrupador" : f.id,
@@ -22,13 +50,20 @@ window.onload = function() {
                   "descricao" : f.short_description.replace(/"/g,"\'").replace(/&nbsp;/g," "),
                   "canal_buscape" : {
                     "canal_url": f.permalink,
-                    "valores":{
-                      "valor":[{
-                        "forma_de_pagamento": "pagseguro",
-                        "pacelamento": "",
-                        "canal_preco": f.price
-                      }]
+                    "valores": {
+                      "valor" : [
+                        {"forma_de_pagamento": "cartao_avista",
+                        "pacelamento": "1x de R$ " + valor_total.toFixed(2),
+                        "canal_preco": "R$ " + valor_total.toFixed(2) },
 
+                        {"forma_de_pagamento": "cartao_parcelado_com_juros",
+                        "pacelamento": "12x de R$ " + valor_parcela_cartao_12x_com_juros.toFixed(2),
+                        "canal_preco": "R$ " + valor_total_cartao_12x_com_juros.toFixed(2)},
+
+                        {"forma_de_pagamento": "cartao_parcelado_sem_juros",
+                        "pacelamento": "3x de R$ " + valor_parcela_cartao_3x_sem_juros.toFixed(2),
+                        "canal_preco": "R$ " + valor_total.toFixed(2)}
+                      ]
                     }
                   },
                   "id_oferta": "",
@@ -49,8 +84,6 @@ window.onload = function() {
                       "atributo":{}
                    }
                   });
-
-                console.log(JSON.stringify(json.produto[value].descricao));
 
                 json.produto[value].imagens.imagem = f.images;
                 
@@ -84,12 +117,12 @@ window.onload = function() {
                   });
                 }
 
-
-                console.log(json.produto[value]);
-
-
                 value++;
         });
-        console.log(JSON.stringify(json));
+
+        //console.log(JSON.stringify(json));
+        //return convertJSon2XML(json);
+
+        console.log(convertJSon2XML(json));
     });
   };
--- index.php
+++ index.php
@@ -3,47 +3,42 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/x2js/1.2.0/xml2json.js"></script>
     <script type="text/javascript" src="assets/js/jquery.min.js"> </script>
+    <script type="text/javascript" src="fileconvert.js"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <title>Json - Converter</title>
   </head>
   <body>
 
-      <form class="" style="width: 50%; padding:100px" method="post">
-        <div class="form-group">
-          <!-- <label for="geturl">URL</label> -->
-          <input type="text" class="form-control" name="" id="urljson" value="" placeholder="URL">
-        </div>
-        <div class="form-group">
-          <!-- <label for="geturl">URL</label> -->
-          <input type="text" class="form-control" name="" id="ck" value="" placeholder="Client Key">
-        </div>
-        <div class="form-group">
-          <!-- <label for="geturl">URL</label> -->
-          <input type="text" class="form-control" name="" id="cs" value="" placeholder="Client Secret Key">
-        </div>
-        <div class="form-group">
-          <!-- <label for="geturl">URL</label> -->
-          <input type="text" class="form-control" name="" id="comando" value="" placeholder="Comando">
-        </div>
-        <button class="btn btn-primary" type="submit">Gerar XML</button>
-      </form>
-    </div>
+  <script type="text/javascript">
+    convertWooToBuscape();
 
-    <!-- <div class="profile">
-    <table id= "userdata" border="2">
-      <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Short</th>
-      </thead>
-      <tbody>
+  </script>
 
-      </tbody>
-    </table>
-    </div> -->
+    <form id="form" style="width: 50%; padding:100px">
+      <div class="form-group">
+        <!-- <label for="geturl">URL</label> -->
+        <input type="text" class="form-control" name="url" id="url" placeholder="URL">
+      </div>
+      <div class="form-group">
+        <!-- <label for="geturl">URL</label> -->
+        <input type="text" class="form-control" name="ck" id="ck" placeholder="Client Key">
+      </div>
+      <div class="form-group">
+        <!-- <label for="geturl">URL</label> -->
+        <input type="text" class="form-control" name="cs" id="cs" placeholder="Client Secret Key">
+      </div>
+      <div class="form-group">
+        <!-- <label for="geturl">URL</label> -->
+        <input type="text" class="form-control" name="cmd" id="cmd" placeholder="Comando">
+      </div>
+      <button class="btn btn-primary" id="send" name="send" type="submit">Gerar XML</button>
+      <button class="btn btn-danger" id="clear" name="clear" type="reset">Limpar</button>
+    </form>
+
   </body>
-  <?php include'getjson.php'; ?>
-  <script type="text/javascript" src="fileconvert.js"></script>
+  
+
+
 </html>
